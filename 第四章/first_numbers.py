636f6eceac470da659c0668346ffc2b106c06d8d
2584650422 <76434074+2584650422@users.noreply.github.com>
2022-10-11 11:14:23 +0800

    Add files via upload

---
+++ 第四章/first_numbers.py
@@ -0,0 +1,6 @@
+for value in range(1,5):
+    print(value)
+for value1 in range(1,6):
+    print(value1)
+for value2 in range(6):
+    print(value2)
---
+++ 第四章/magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice','david','carolina']
+for magician in magicians:
+#     print(magician)
+     print(f"{magician.title()},that was a great trick!")
+     print(f"I cat't wait to see your next trick, {magician.title()}.\n")
+#print()缩进会重复执行，不缩进只会执行一次，不会重复执行
+print("Thank you, everyone. That was a great magic show!")
